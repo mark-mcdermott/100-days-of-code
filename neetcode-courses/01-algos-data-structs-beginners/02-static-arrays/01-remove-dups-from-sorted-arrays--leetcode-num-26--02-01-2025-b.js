9aeba0373bd4008503bf5404684d7b388bee6ef6
Mark McDermott <mark@markmcdermott.io>
2025-02-02 11:35:48 -0600

    Add neetcode

---
+++ neetcode-courses/01-algos-data-structs-beginners/02-static-arrays/01-remove-dups-from-sorted-arrays--leetcode-num-26--02-01-2025-b.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function(nums) {
+  let k = 1
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[k++] = nums[i]
+    }
+  }
+  return k
+}
