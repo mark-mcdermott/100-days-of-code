a3807893dfcb0bcce6485f6e848ba56398a6df47
mark-mcdermott <mark@markmcdermott.io>
2025-02-07 04:17:59 -0600

    Finish reversing singly linked list problem

--- neetcode-courses/01-algos-data-structs-beginners/05-singly-linked-lists/01-reverse-linked-list--neetcode-150--02-03-2025.js
+++ neetcode-courses/01-algos-data-structs-beginners/05-singly-linked-lists/01-reverse-linked-list--neetcode-150--02-03-2025.js
@@ -1,48 +1,49 @@
 const { ListNode, SinglyLinkedList } = require('./SinglyLinkedList.js')
 
 class Solution {
-  getTail(head) {
-    let curr = head
-    while (curr.next != null) {
-      curr = curr.next
-    }
-    return curr
-  }
-
   /**
    * @param {ListNode} head
    * @return {ListNode}
    */
   reverseList(head) {
+    let prev = null
     let curr = head
-    let next = head.next
-    while (next != null) {
-      
+
+    while (curr) {
+      let temp = curr.next
+      curr.next = prev
+      prev = curr
+      curr = temp
     }
-    // let reversedHead = new SinglyLinkedList(head.)
-    // let curr = head
-    // do {
+    return prev
+  }
 
-    // } while (curr.next != null) {
+  getInitialList() {
+    let list = new SinglyLinkedList(0)
+    list.insertTail(1)
+    list.insertTail(2)
+    list.insertTail(3)
+    list.insertTail(4)
+    return list
+  }
 
-    // return newHead
+  getTail(head) {
+    let curr = list.head
+    while (curr.next) {
+      curr = curr.next
+    }
+    return curr
   }
 }
 
-let list = new SinglyLinkedList(0)
-list.insertTail(1)
-list.insertTail(2)
-list.insertTail(3)
-list.insertHead(4)
+const solution = new Solution
+const list = solution.getInitialList()
 console.log(`initial list: ${list.getValues()}`)
 
-const solution = new Solution
-const reversedList = solution.reverseList(list)
+const reversedList = solution.reverseList(list.head)
+list.head = reversedList
+list.tail = solution.getTail(list.head)
 
-// const reversedArr = reversedList.getValues()
-// console.log(`reversed list: ${reversedArr}`)
-
-
-// let solution = new Solution(list)
-// // console.log(`reversed list: ${solution.reverseList(list.getValues())}`)
-// console.log(`reversed list: ${solution.reverseList(head)}`)
+console.log(`reversed list: ${list.getValues()}`)
+console.log(`new head: ${list.head.val}`)
+console.log(`new tail: ${list.tail.val}`)
