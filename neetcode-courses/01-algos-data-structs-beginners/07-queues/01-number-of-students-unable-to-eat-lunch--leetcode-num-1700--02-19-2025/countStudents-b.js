0342a7d317f887dbcb97f5a4bf021f824e3ff2e8
mark-mcdermott <mark@markmcdermott.io>
2025-02-19 23:44:13 -0600

    Wrote up neetcode answer to 'number of students unable to eat lunch' problem

--- neetcode-courses/01-algos-data-structs-beginners/07-queues/01-number-of-students-unable-to-eat-lunch--leetcode-num-1700--02-19-2025/countStudents-b.js
+++ neetcode-courses/01-algos-data-structs-beginners/07-queues/01-number-of-students-unable-to-eat-lunch--leetcode-num-1700--02-19-2025/countStudents-b.js
@@ -1,19 +1,8 @@
 class Queue {
-  constructor() {
-    this.items = [];
-  }
-  
-  push(val) {
-    this.items.push(val);
-  }
-  
-  pop() {
-    return this.items.shift();
-  }
-  
-  front() {
-    return this.items.length > 0 ? this.items[0] : null;
-  }
+  constructor() { this.items = [] }
+  push(val) { this.items.push(val) }
+  pop() { return this.items.shift() }
+  front() { return this.items.length > 0 ? this.items[0] : null }
 }
 
 /**
@@ -22,7 +11,26 @@ class Queue {
  * @return {number}
  */
 var countStudents = function(students, sandwiches) {
-  // TODO
+  const n = students.length
+  let q = new Queue()
+  for (let student of students) {
+    q.push(student)
+  }
+  let res = n
+  for (let sandwhich of sandwiches) {
+    let cnt = 0
+    while (cnt < n && q.front() != sandwhich) {
+      q.push(q.pop())
+      cnt++
+    }
+    if (q.front() == sandwhich) {
+      q.pop()
+      res--
+    } else {
+      break
+    }
+  }
+  return res
 };
 
 console.log('countStudents([1,1,0,0], [0,1,0,1]): ' + countStudents([1,1,0,0], [0,1,0,1])) // 0
